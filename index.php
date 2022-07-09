9026a4fcbd91f1070bde684879bc9a2a066b005f
hasanfardous <frworkbd@gmail.com>
2022-07-09 10:40:19 +0600

    Basic theme development - class 2

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<h1>Welcome! To our first class</h1>
+<?php
+$args = array(
+    'post_type' => 'post',
+    'order' => 'ASC',
+    'posts_per_page' => 2,
+);
+$query = new WP_Query( $args );
+
+// echo '<pre>';
+// print_r( $query );
+
+while ($query->have_posts()) {
+    $query->the_post();
+    the_title();
+    the_content();
+    the_post_thumbnail('thumbnail');
+    echo '<hr>';
+}
+?>
