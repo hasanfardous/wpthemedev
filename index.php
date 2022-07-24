7b2de3f7ecff61216ace58fb22a195b6aa8dace3
hasanfardous <frworkbd@gmail.com>
2022-07-24 09:44:39 +0600

    Class 6 - different post for odd even item

--- index.php
+++ index.php
@@ -5,17 +5,60 @@
     $args = array(
         'post_type' => 'post',
         'order' => 'ASC',
-        'posts_per_page' => 2,
+        // 'posts_per_page' => 2,
     );
     $query = new WP_Query( $args );
+    ?>
 
-    while ($query->have_posts()) {
-        $query->the_post();
-        the_title();
-        the_content();
-        the_post_thumbnail('thumbnail');
-        echo '<hr>';
-    }
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="post-area">
+                    <?php
+                    $counter = 0;
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        $counter++;
+                        if ( $counter % 2 == 0 ) {
+                        ?>
+                        <div class="row single-post">
+                            <div class="post-thumbnail col-sm-6">
+                                <?php
+                                    the_title();
+                                    the_content();
+                                ?>
+                            </div>
+
+                            <div class="post-content col-sm-6">
+                                <?php the_post_thumbnail('thumbnail');?>
+                            </div>
+                        </div>
+                        <?php
+                        } else {
+                        ?>
+                        <div class="row single-post">
+                            <div class="post-thumbnail col-sm-6">
+                                <?php the_post_thumbnail('thumbnail');?>
+                            </div>
+
+                            <div class="post-content col-sm-6">
+                                <?php
+                                    the_title();
+                                    the_content();
+                                ?>
+                            </div>
+                        </div>
+                        <?php
+                        }
+                        ?>
+                        
+                        <?php
+                    }?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
 
     // Get footer content
     get_footer();
